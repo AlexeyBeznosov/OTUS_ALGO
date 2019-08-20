6c1d60ba8c80d98fa24e994e10c764910688413f
AlexeyBeznosov <33223639+AlexeyBeznosov@users.noreply.github.com>
2019-08-20 07:41:13 +0300

    Add files via upload

---
+++ 05_SHELL/InsertSort.java
@@ -0,0 +1,16 @@
+
+public class InsertSort implements ISort {
+
+    @Override
+    public void sort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i;
+            while (j > 0 && arr[j - 1] > temp) {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+            arr[j] = temp;
+        }
+    }
+}
